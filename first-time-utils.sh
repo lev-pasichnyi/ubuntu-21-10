533c57770cd9f71aa2936863b9375afda20577d0
Lev Pasichnyi <lev.pasichnyi@gmail.com>
2022-01-01 15:09:10 +0200

    Update first-time-utils.sh

--- first-time-utils.sh
+++ first-time-utils.sh
@@ -1,5 +1,10 @@
-# upgrade all packages in Ubuntu
+# upgrade all packages in Ubuntu for first time use 
 sudo apt update && sudo apt upgrade
 
 # install missed fonts and codecs
 sudo apt install ubuntu-restricted-extras
+
+# install video player and video codecs
+# I prefer mpv, but you could use vlc: sudo apt install vlc
+sudo apt install mpv
+
