6a5a81c121fd2b92da1a077c8be40182cf102399
Lev Pasichnyi <lev.pasichnyi@gmail.com>
2022-01-09 16:18:06 +0200

    Update first-time-utils.sh

--- first-time-utils.sh
+++ first-time-utils.sh
@@ -14,6 +14,18 @@ sudo apt -y install ./viber.deb
 echo " installing Telegram messenger "
 sudo snap install telegram-desktop
 
+echo " installing KVM virtualization"
+# number of CPU with virtualization capabilities
+egrep -c '(vmx|svm)' /proc/cpuinfo
+sleep 4
+sudo apt -y install cpu-checker
+sudo apt update
+sudo apt -y install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
+sudo adduser $USER kvm
+sudo systemctl status libvirtd
+sleep 4
+sudo apt -y install virt-manager
+
 echo " cleaning up after bootstrapping..."
 sudo apt -y autoremove
 sudo apt -y clean
